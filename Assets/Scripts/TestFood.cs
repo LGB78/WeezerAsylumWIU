85b04ad41d1bdc43a40db17a957174ca50819767
julestheadvisor <wafflesthereshiram632@gmail.com>
2025-08-19 16:49:14 +0800

    UPDATE FOR TESTFOOD.CS
    added some comments mainly and now the food snaps to the middle of the plate if put down on plate
    
    where u want it to snap to exactly can be adjusted with new Vector2(x, y) stuff once we get to actually arranging food

--- Assets/Scripts/TestFood.cs
+++ Assets/Scripts/TestFood.cs
@@ -25,10 +25,14 @@ public class TestFood : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if it's not plated, then check for mouse button down and such
+        //if its plated then dont cause u cant move it anymore
+        //same logic goes for the onmousedrag part
         if (!isPlated)
         {
             if (Input.GetMouseButton(0))
             {
+                //raycast from mouse position and see if it click object
                 Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
                 RaycastHit2D hit = Physics2D.Raycast(mouseRay.origin, mouseRay.direction);
                 if (hit.collider == thiscollider)
@@ -42,16 +46,26 @@ public class TestFood : MonoBehaviour
             }
             else if (Input.GetMouseButtonUp(0))
             {
+                //if it wasnt dropped on the plate then put it back at the start
                 if (!IsTouchingMouse(plate))
                 {
                     transform.position = startPosition;
                 }
                 else
                 {
+                    //i realised that if i didnt check if its the object clicked then
+                    //both objects would be clicked. so this is just to check for
+                    //the actual objec being clicked
+                    //tbh the mouse code could probably be moved to another file but
                     if (isClicked)
-                    { 
+                    {
                         food.Plate(this.gameObject);
                         isPlated = true;
+                        transform.position = plate.transform.position;
+                        //plate food (prevent further movement) and snap to plate position
+                        //the last line about transform.position can be changed as desired 
+                        //for different food snapping once we have the actual plate sprite and we're
+                        //arranging the food
                     }
                 }
             }
@@ -67,12 +81,14 @@ public class TestFood : MonoBehaviour
 
     }
 
+    //code that checks for if the mouse is inside the plate's collider area
     public bool IsTouchingMouse(GameObject g)
     {
         Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         return g.GetComponent<Collider2D>().OverlapPoint(point);
     }
 
+    //code that drags clicked object while mouse is down.
     public void DragObject()
     {
         Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
