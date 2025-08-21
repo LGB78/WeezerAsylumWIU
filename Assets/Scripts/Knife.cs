31059df7a4e55cbd9d769eb5205242e8062e9758
julestheadvisor <wafflesthereshiram632@gmail.com>
2025-08-21 10:01:24 +0800

    some changes to knife code (inscomplete)

--- Assets/Scripts/Knife.cs
+++ Assets/Scripts/Knife.cs
@@ -3,30 +3,62 @@ using UnityEngine;
 public class Knife : MonoBehaviour
 {
     private bool isClicked;
+    private DraggableObject draggable;
+    private Vector2 startPosition;
+
+    public GameObject board;
+    public GameObject food;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         isClicked = false;
+        draggable = GetComponent<DraggableObject>();
+        startPosition = transform.position;
     }
 
     private void OnMouseDown()
     {
-        /*if (food == null || heldObj != null) return;
+        isClicked = !isClicked;
+        if (!isClicked)
+        {
+            if (IsTouchingMouse(board))
+            {
+                isClicked = true;
+                if (board.transform.GetChild(0).GetChild(0).gameObject != null)
+                {
+                    food = board.transform.GetChild(0).GetChild(0).gameObject;
+                    if (IsTouchingMouse(food))
+                    {
+                        var foodrenderer = food.GetComponent<SpriteRenderer>();
 
-        // Spawn the held food sprite object
-        heldObj = new GameObject("Held_" + food.foodName);
-
-        var sr = heldObj.AddComponent<SpriteRenderer>();
-        sr.sprite = food.unplatedFoodSprite;
-        sr.sortingOrder = 69; // render above everything
-
-        //lucas drag script
-        draggable = heldObj.AddComponent<DraggableObject>();*/
+                    }
+                }
+            }
+            else
+            {
+                transform.position = startPosition;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isClicked)
+        {
+            draggable.DragObject();
+        }
+    }
+
+    public bool IsTouchingMouse(GameObject obj)
+    {
+        Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return obj.GetComponent<Collider2D>().OverlapPoint(point);
+    }
+
+    private void CutFood(GameObject obj)
+    {
+
     }
 }
