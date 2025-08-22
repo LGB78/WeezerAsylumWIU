d53fa9fdeddcfbad4ea49d6f219dddc50ae36fdf
julestheadvisor <wafflesthereshiram632@gmail.com>
2025-08-22 10:57:22 +0800

    Functioning timer and added Scene Loader
    Also just adjust maxtime to whatever u want 10 second is placeholder for testing

--- Assets/Scripts/daytimer.cs
+++ Assets/Scripts/daytimer.cs
@@ -5,16 +5,26 @@ public class daytimer : MonoBehaviour
 {
     public UnityEvent dayover;
     [SerializeField] float maxtime;
+    private Transform t;
+    private float currentTime;
+    public GameObject clockHand;
     void Start()
     {
+        t = clockHand.GetComponent<RectTransform>();
+        currentTime = maxtime;
     }
 
     void Update()
     {
-        maxtime -= Time.deltaTime;
-        if (maxtime <= 0 )
+        if (currentTime <= 0 )
         {
             dayover.Invoke();
+            enabled = false;
+        }
+        else
+        {
+            currentTime -= Time.deltaTime;
+            t.localRotation = Quaternion.Euler(0, 0, currentTime / maxtime * 360);
         }
     }
 }
