49bb84e204dba839032f34dab2d6649b479edfbd
LGB78 <lucasgoh78@gmail.com>
2025-08-25 11:40:09 +0800

    wombo combo

---
+++ Assets/Scripts/Combo.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+public class Combo : MonoBehaviour
+{
+    public int combo;
+    TMP_Text text;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        combo = 0;
+        text = GetComponent<TMP_Text>();
+        updatetext();
+    }
+    public void updatetext()
+    {
+        text.text = "Combo: " + combo;
+    }
+    
+}
